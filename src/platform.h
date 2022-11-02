3614ca45b9835a6581e7080e4644b9cbc19312cd
KarolinaSuliga <karolina.suliga98@gmail.com>
2022-11-02 20:27:45 +0100

    removed invalid platform config

--- src/platform.h
+++ src/platform.h
@@ -31,9 +31,6 @@
 
 void init_platform();
 void cleanup_platform();
-#ifdef __MICROBLAZE__
-void timer_callback();
-#endif
 void platform_setup_timer();
 void platform_enable_interrupts();
 u64_t get_time_ms();
